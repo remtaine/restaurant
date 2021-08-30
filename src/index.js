980081a92225626b1ddce4f3ec487f304a3dc63b
Benjo Kho <benjokho@gmail.com>
2021-08-30 16:31:03 +0800

    Import other js files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
+import createFront from './front.js';
+import createMenu from './menu.js';
+import createContact from './contact.js';
+
 import './styles/normalize.css';
 import './styles/style.scss';
+import './images/food1.jpg';
+
+let content = document.getElementById('content');
+
+// createMenu();
+// createContact();
+// createFront();
+
+const removeAllChildNodes = function(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+let tabHome = document.getElementById('tab-home');
+let tabMenu = document.getElementById('tab-menu');
+let tabContact = document.getElementById('tab-contact');
+
+// removeAllChildNodes(content);
+// createFront();
+
+tabHome.addEventListener('click', function() {
+    removeAllChildNodes(content);
+    createFront();
+});
