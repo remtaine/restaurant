cf00085d07cac48a53d31c04b5c912f7e6444ad2
Benjo Kho <benjokho@gmail.com>
2021-08-30 18:08:32 +0800

    Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    front: './src/front.js',
+    menu: './src/menu.js',
+    contact: './src/contact.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Malasa Lounge",
+      filename: 'index.html',
+      template: './src/start.html',
+      favicon: './src/images/favicon.ico',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
@@ -27,6 +42,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
