c9fc0cbe55743047c4ebe55cc948a248ae754310
Benjo Kho <benjokho@gmail.com>
2021-08-30 19:28:25 +0800

    Add active tab setter

--- src/index.js
+++ src/index.js
@@ -9,35 +9,43 @@ import './images/food2.jpg';
 import './images/food3.jpg';
 
 let content = document.getElementById('content');
-
-// createMenu();
-// createContact();
-// createFront();
+let tabHome = document.getElementById('tab-home');
+let tabMenu = document.getElementById('tab-menu');
+let tabContact = document.getElementById('tab-contact');
 
 const removeAllChildNodes = function(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
-}
+};
 
-let tabHome = document.getElementById('tab-home');
-let tabMenu = document.getElementById('tab-menu');
-let tabContact = document.getElementById('tab-contact');
+const setActiveTab = function(tab) {
+    tabContact.classList.remove('active');
+    tabHome.classList.remove('active');
+    tabMenu.classList.remove('active');
+    tab.classList.add('active');
+};
+
+
 
 removeAllChildNodes(content);
 createFront();
+setActiveTab(tabHome);
 
 tabHome.addEventListener('click', function() {
     removeAllChildNodes(content);
     createFront();
+    setActiveTab(tabHome);
 });
 
 tabMenu.addEventListener('click', function() {
     removeAllChildNodes(content);
     createMenu();
+    setActiveTab(tabMenu);
 });
 
 tabContact.addEventListener('click', function() {
     removeAllChildNodes(content);
     createContact();
+    setActiveTab(tabContact);
 });
